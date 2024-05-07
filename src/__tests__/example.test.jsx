99a868eb2af5bd531a91364cc5e5a7fc1ab86900
Luis Arteaga <arteagaluis123@gmail.com>
2024-05-07 18:44:44 -0500

    re run github action 2

--- src/__tests__/example.test.jsx
+++ src/__tests__/example.test.jsx
@@ -1,8 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import Menu from '../components/Menu';
 
-//borrar
-
 test('renders example component', () => {
   render(<Menu />);
   screen.debug();
