43722aafd2bfeb7c2070877ed513f9de39dbc16e
Luis Arteaga <arteagaluis123@gmail.com>
2024-05-08 13:21:48 -0500

    Config unit test

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,8 +1,8 @@
 'use client';
 import Image from 'next/image';
 import { useState } from 'react';
-// import homeIcon from '../../../public/img/icon/home-icon.svg';
-// import homeIconBlue from '../../../public/img/icon/home-icon-blue.svg';
+import homeIcon from '../../../public/img/icon/home-icon.svg';
+import homeIconBlue from '../../../public/img/icon/home-icon-blue.svg';
 import Link from 'next/link';
 
 const Menu = () => {
@@ -12,12 +12,12 @@ const Menu = () => {
     <>
       <nav>
         <Link href={'/dashboard/home'}>
-          {/* <Image
+          <Image
             src={select ? homeIconBlue : homeIcon}
             width={24}
             height={24}
             alt="icon-home"
-          /> */}
+          />
           Home
         </Link>
       </nav>
