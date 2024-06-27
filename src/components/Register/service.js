d343437c430dc84ca6016da9056dd48314d6194d
Luis Arteaga <arteagaluis123@gmail.com>
2024-06-27 12:39:24 -0500

    cambios varios

--- src/components/Register/service.js
+++ src/components/Register/service.js
@@ -1,10 +1,12 @@
 import fecthService from '@/util/FecthService.js';
 
 export const servicioRegitroUsuarios = async (datos) => {
+  console.log(process.env.NEXT_PUBLIC_SRV_REGISTER);
   try {
     const data = await fecthService({
+      url: process.env.NEXT_PUBLIC_SRV_REGISTER,
       // url: 'http://backend:3001/api/register',
-      url: 'http://18.231.243.156:4000/api/register',
+      // url: 'http://18.231.243.156:4000/api/register',
       method: 'POST',
       data: {
         email: datos.email,
