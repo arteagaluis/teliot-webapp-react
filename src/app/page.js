bd04a4e1541aeeb469cdf24d0425a48c87fdaa44
Luis Arteaga <arteagaluis123@gmail.com>
2024-08-17 11:21:25 -0500

    redirect login

--- src/app/page.js
+++ src/app/page.js
@@ -7,29 +7,13 @@ import LayoutInit from '@/components/ToggleSwitch/LayoutInit';
 import styles from './styles.module.scss';
 import Image from 'next/image';
 import Input from '@/components/Input';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
 export default function Home() {
-  return (
-    <>
-      <LayoutInit>
-        <Image
-          src="/img/logo.svg"
-          width={200}
-          height={200}
-          alt="Picture of the author"
-        />
-        <Register />
-      </LayoutInit>
+  const router = useRouter();
 
-      <Input
-        name={'fullName'}
-        placeholder={'Nombre completo'}
-        // onChange={handleChange}
-        type={'text'}
-        // icon={'img/icon/user-icon.svg'}
-        // value={form.fullName}
-        className={styles.input}
-      />
-    </>
-  );
+  useEffect(() => {
+    return router.push('/login');
+  }, []);
 }
