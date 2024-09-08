4156daaec3c7c8b4466bafc45db03002378a74b0
Luis Arteaga <arteagaluis123@gmail.com>
2024-09-08 00:42:48 -0500

    Pruebas pipeline

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,4 @@
 'use client';
-
-import Register from '@/components/Register';
-import Login from '@/components/Login';
-import ToggleSwitch from '@/components/ToggleSwitch';
-import LayoutInit from '@/components/ToggleSwitch/LayoutInit';
-import styles from './styles.module.scss';
-import Image from 'next/image';
-import Input from '@/components/Input';
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 
