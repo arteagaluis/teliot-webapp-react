27004bc6a4a47f8ef229be14a30b8cd0a7112cc1
Luis Arteaga <arteagaluis123@gmail.com>
2024-11-04 19:07:39 -0500

    ajuste mqtt se agrego url de prod

--- src/app/dashboard/home/page.js
+++ src/app/dashboard/home/page.js
@@ -1,9 +1,29 @@
-import React from 'react';
+'use client';
+import { connectToBroker } from '@/util/mqttClient';
+import React, { useEffect, useState } from 'react';
 
 const HomePage = () => {
+  const [mensaje, setMensaje] = useState('');
+
+  useEffect(() => {
+    const client = connectToBroker();
+
+    client.on('message', (topic, message) => {
+      const mensajeRecibido = message.toString();
+      setMensaje(mensajeRecibido); // Actualiza el estado con el mensaje recibido
+    });
+
+    // Limpia la conexión cuando el componente se desmonta
+    return () => {
+      client.end(); // Cierra la conexión al desmontar el componente
+    };
+  }, []);
+
   return (
     <>
       <div>HomePage</div>
+      <h2>Suscriptor MQTT</h2>
+      <p>temperatura: {mensaje} </p>
     </>
   );
 };
---
+++ src/util/mqttClient.js
@@ -0,0 +1,26 @@
+import mqtt from 'mqtt';
+
+export function connectToBroker() {
+  const client = mqtt.connect('ws://iot-test.online:8083/mqtt'); // Reemplaza con la URL de tu broker
+
+  client.on('connect', () => {
+    console.log('Conectado al broker MQTT');
+    client.subscribe('testtopic/temperatura', (err) => {
+      if (!err) {
+        console.log('Suscripción exitosa al topic');
+      } else {
+        console.error('Error al suscribirse:', err);
+      }
+    });
+  });
+
+  client.on('message', (topic, message) => {
+    console.log(`Mensaje recibido en ${topic}: ${message.toString()}`);
+  });
+
+  client.on('error', (err) => {
+    console.error('Error de conexión:', err);
+  });
+
+  return client;
+}
